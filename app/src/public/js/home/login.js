5f1712570f2c07715b40c08bcd247c0e25f342e4
unknown <huh102400@gmail.com>
2024-05-16 17:34:49 +0900

    login module start

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,3 +1,15 @@
 "use strict";
 
-console.log("hello");
+const id = document.querySelector("#id"),
+    psword = document.querySelector("#psword"),
+    loginBtn = document.querySelector("button");
+
+console.log(id);
+loginBtn.addEventListener("click", login);
+
+function login(){
+    const req = {
+        id : id.ariaValue,
+        psword : psword.value,
+    }
+}
